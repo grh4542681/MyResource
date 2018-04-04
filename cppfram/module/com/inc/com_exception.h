db6da7354d3740132b6ab5b9df6e167be47617de
RonghuaGao <grh454268@163.com>
2018-04-04 19:26:18 +0800

    com

---
+++ cppfram/module/com/inc/com_exception.h
@@ -0,0 +1,14 @@
+#ifndef __COM_EXCEPTION_H__
+#define __COM_EXCEPTION_H__
+
+class ComException : public Global::CommException{
+public:
+    ComException(int);
+    ComException(std::exception&);
+    ~ComException() throw();
+}
+
+
+
+
+#endif
--- cppfram/module/com/inc/communication.h
+++ cppfram/module/com/inc/communication.h
@@ -2,5 +2,13 @@
 #define __COMMUNICATION_H__
 
 class BaseCommunication{
-
+public:
+    BaseCommunication();
+    ~BaseCommunication();
+    virtual void open() throw(...);
+    virtual void close() throw(...);
+    virtual void write() throw(...);
+    virtual void read() throw(...);
 }
+
+#endif
